0203fe9d65419a799692bcb0af1688eb762cfb2f
Zo-Tsilavina <hei.tsilavina@gmail.com>
2023-11-19 22:20:40 +0300

    added environment variable configurations

---
+++ src/JDBC/ConnectionDB.java
@@ -0,0 +1,25 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    private Connection connection;
+
+    public ConnectionDB(){
+        try{
+            String dbUrl = System.getenv("DB_URL");
+            String dbUser = System.getenv("DB_USER");
+            String dbPassword = System.getenv("DB_PASSWORD");
+
+            this.connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+}
